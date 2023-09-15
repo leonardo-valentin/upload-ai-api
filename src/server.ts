4c4efc3956449a9927f912540ac9cc4ccda4ee7d
Leonardo Valentin dos Santos <71048056+leonardo-valentin@users.noreply.github.com>
2023-09-15 17:14:49 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 const app = fastify();
 
 app.register(fastifyCors, {
-  origin: "*",
+  origin: true,
 });
 
 app.register(getAllPromptsRoute);
@@ -18,7 +18,7 @@ app.register(generateAICompletionRoute);
 
 app
   .listen({
-    port: 3333,
+    port: process.env.PORT :: 3333,
   })
   .then(() => {
     console.log("HTTP SERVER RUNNING");
